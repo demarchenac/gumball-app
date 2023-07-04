f933b6a1ec965d1f4b6c628828de4af651b7f790
Cristhyan D. Marchena <crdemar@gmail.com>
2023-07-04 01:42:38 -0500

    chore: add conventional commits & fix config extensions

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,4 @@
-const lintStagedConfig = {
+module.exports = {
   '*.{csj,mjs,html,css,md}': ['prettier --write'],
   '*.{ts,tsx}': ['prettier --write', 'bash -c "pnpm lint:types"', 'eslint --fix'],
 };
-
-export default lintStagedConfig;
