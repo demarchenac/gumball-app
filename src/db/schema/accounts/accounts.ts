9e13a96e6caa60772b1850167dde9a8be53b4e49
Cristhyan D. Marchena <crdemar@gmail.com>
2023-07-30 21:07:52 -0500

    chore: some drizzle columns were wrong

--- src/db/schema/accounts/accounts.ts
+++ src/db/schema/accounts/accounts.ts
@@ -1,4 +1,4 @@
-import { int, mysqlTable, primaryKey, varchar } from 'drizzle-orm/mysql-core';
+import { int, mysqlTable, primaryKey, text, varchar } from 'drizzle-orm/mysql-core';
 import type { AdapterAccount } from 'next-auth/adapters';
 
 export const accounts = mysqlTable(
@@ -8,13 +8,13 @@ export const accounts = mysqlTable(
     type: varchar('type', { length: 255 }).$type<AdapterAccount['type']>().notNull(),
     provider: varchar('provider', { length: 255 }).notNull(),
     providerAccountId: varchar('providerAccountId', { length: 255 }).notNull(),
-    refresh_token: varchar('refresh_token', { length: 255 }),
-    access_token: varchar('access_token', { length: 255 }),
+    refresh_token: text('refresh_token'),
+    access_token: text('access_token'),
     expires_at: int('expires_at'),
     token_type: varchar('token_type', { length: 255 }),
     scope: varchar('scope', { length: 255 }),
-    id_token: varchar('id_token', { length: 255 }),
-    session_state: varchar('session_state', { length: 255 }),
+    id_token: text('id_token'),
+    session_state: text('session_state'),
   },
   (account) => ({ compoundKey: primaryKey(account.provider, account.providerAccountId) })
 );
